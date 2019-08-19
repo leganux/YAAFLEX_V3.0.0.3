15ecbeae8cee37507f4fed93667fa2acbf9f2be4
Angel Erick Cruz Olivera <leganux@hotmail.com>
2019-08-18 22:07:04 -0500

    Token
    Token

--- src/auth/logintoken.js
+++ src/auth/logintoken.js
@@ -1,30 +1,173 @@
 const express = require('express');
 const router = express.Router();
+var jwt = require('jsonwebtoken');
+
+const Admin = require('../models/NOSQL/admin');
+const User = require('../models/NOSQL/user.model');
+const bcrypt = require('bcryptjs');
+const env = require('./../config/environment.config')
+const global = require('./../config/global.config')
 
 
-app.post('/login', (req, res) => {
-    var username = req.body.user
+router.post('/getAdminToken', (req, res) => {
+    var username = req.body.username
     var password = req.body.password
 
-    if (!(username === 'oscar' && password === '1234')) {
-        res.status(401).send({
-            error: 'usuario o contraseña inválidos'
-        })
-        return
+    Admin.findOne({username: username}, function (err, user) {
+        if (err) {
+            return res.status(500).json({
+                message: '500 Internal Server Error',
+                error: err,
+                success: false
+            })
+        }
+        if (!user) {
+            return res.status(404).json({
+                message: '404 Object not found',
+                success: false
+            });
+        }
+
+        bcrypt.compare(password, user.password, function (err, rsp) {
+            if (err) {
+                if (err) {
+                    return res.status(500).json({
+                        message: '500 Internal Server Error',
+                        error: err,
+                        success: false
+                    })
+                }
+            }
+            if (!rsp) {
+                return res.status(404).json({
+                    message: '404 Object not found',
+                    success: false
+                })
+
+            }
+            user.isAdmin = true;
+            var tokenData = {
+                session: user,
+                prop: {
+                    kind: 'admin',
+                    role: user.role
+                }
+            };
+
+
+            var token = jwt.sign(tokenData, global.JWT_Secret, {expiresIn: '1h'});
+
+            return res.status(200).json({
+                message: 'OK',
+                token,
+                success: true
+            });
+
+
+        });
+
+
+    });
+
+
+});
+
+router.post('/getUserToken', (req, res) => {
+    var username = req.body.username
+    var password = req.body.password
+
+    User.findOne({username: username}, function (err, user) {
+        if (err) {
+            return res.status(500).json({
+                message: '500 Internal Server Error',
+                error: err,
+                success: false
+            })
+        }
+        if (!user) {
+            return res.status(404).json({
+                message: '404 Object not found',
+                success: false
+            });
+        }
+
+        bcrypt.compare(password, user.password, function (err, rsp) {
+            if (err) {
+                if (err) {
+                    return res.status(500).json({
+                        message: '500 Internal Server Error',
+                        error: err,
+                        success: false
+                    })
+                }
+            }
+            if (!rsp) {
+                return res.status(404).json({
+                    message: '404 Object not found',
+                    success: false
+                })
+
+            }
+            user.isAdmin = false;
+            var tokenData = {
+                session: user,
+                prop: {
+                    isAdmin: false,
+                }
+            };
+
+
+            var token = jwt.sign(tokenData, global.JWT_Secret, {expiresIn: '1h'});
+
+            return res.status(200).json({
+                message: 'OK',
+                token,
+                success: true
+            });
+
+
+        });
+
+
+    });
+
+
+});
+
+router.get('/verify', (req, res) => {
+    var authToken = req.headers.authorization;
+
+    if (!authToken) {
+        return res.status(404).json({
+            message: '404 Token not set in authorization token',
+            success: false
+        });
     }
 
-    var tokenData = {
-        username: username
-        // ANY DATA
-    }
+    jwt.verify(authToken, global.JWT_Secret, function (err, decoded) {
+        if (err) {
+            return res.status(500).json({
+                message: '500 Internal Server Error',
+                error: err,
+                success: false
+            })
+        }
+        if (!decoded) {
+            return res.status(404).json({
+                message: '404 Object not found',
+                success: false
+            });
+        }
 
-    var token = jwt.sign(tokenData, 'Secret Password', {
-        expiresIn: 60 * 60 * 24 // expires in 24 hours
-    })
+        return res.status(200).json({
+            message: 'OK',
+            decoded,
+            success: true
+        });
 
-    res.send({
-        token
-    })
-})
+    });
+
+
+});
 
 module.exports = router;
